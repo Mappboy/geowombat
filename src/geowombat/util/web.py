fb8281e45386542fb7c2d6389cad854ce44842af
Cameron Poole <cameron@geoneon.com>
2023-12-19 15:15:09 +1100

    :bug: Fix geodownload input dates

--- src/geowombat/util/web.py
+++ src/geowombat/util/web.py
@@ -1190,8 +1190,8 @@ class GeoDownloads(CloudPathMixin, DownloadMixin):
 
             shp_dict['mgrs'] = df_mgrs
 
-        dt1 = datetime.strptime(date_range[0], '%Y-%m')
-        dt2 = datetime.strptime(date_range[1], '%Y-%m')
+        dt1 = datetime.strptime(date_range[0], '%Y-%m-%d')
+        dt2 = datetime.strptime(date_range[1], '%Y-%m-%d')
 
         months = list(range(1, 13))
         year_months = {}
